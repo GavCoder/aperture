4564b94dc87b2417057088fba90b76e643d39e7b
Karabo Shopane <zephyr.shopane@gmail.com>
2026-03-05 21:22:32 +0200

    Removed Safe Area under Scafold and Padding from Column children in Feed screen

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -9,105 +9,99 @@ class FeedScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.scaffoldBackground,
+      // Removed SafeArea to allow background blobs to extend to edges of screen
       body: Stack(
         children: [
-          SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40, left: 18, right: 18),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text.rich(
-                        TextSpan(
-                          style: TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.textWhite,
-                          ),
-                          children: [
-                            TextSpan(
-                                text: 'Welcome, ',
-                                style:
-                                    TextStyle(fontWeight: FontWeight.normal)),
-                            TextSpan(
-                              text: 'Gavin',
-                              style: TextStyle(
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: const Icon(
-                          Icons.person,
-                          color: AppColors.iconWhite,
-                          size: 28,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: 5,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 10, vertical: 20),
-                        child: Container(
-                          // Need to extract and turn into PostCard widget
-                          height: 550,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: AppColors.gradientMediumPurple,
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: ClipPath(
-                            clipper: ShapeBorderClipper(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              ),
-                            ),
-                            child: Image.asset(
-                              'assets/images/Skinny Dipping.png',
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                FloatingActionButton(
-                  elevation: 10,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  onPressed: () {},
-                  backgroundColor: AppColors.gradientMediumPurple,
-                  child: const Icon(
-                    Icons.add_rounded,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          
           Positioned(
-            top: 50,
-            right: 50,
+            top: -200,
+            left: -50,
             child: SvgPicture.asset(
               'assets/blobs/blob1.svg',
-              width: 200,
+              width: 320,
             ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text.rich(
+                    TextSpan(
+                      style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.textWhite,
+                      ),
+                      children: [
+                        TextSpan(
+                            text: 'Welcome, ',
+                            style: TextStyle(fontWeight: FontWeight.normal)),
+                        TextSpan(
+                          text: 'Gavin',
+                          style: TextStyle(
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.person,
+                      color: AppColors.iconWhite,
+                      size: 28,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 14, vertical: 10),
+                      child: Container(
+                        // Need to extract and turn into PostCard widget
+                        height: 550,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: AppColors.gradientMediumPurple,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: ClipPath(
+                          clipper: ShapeBorderClipper(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: Image.asset(
+                            'assets/images/Skinny Dipping.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              FloatingActionButton(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                onPressed: () {},
+                backgroundColor: AppColors.gradientMediumPurple,
+                child: const Icon(
+                  Icons.add_rounded,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ),
+            ],
           ),
         ],
       ),
